11815147d1f2d64a83aee50226a94075fea9eeed
Danny Eck <danny.eck@osi.ca.gov>
2018-06-13 14:45:01 -0700

    Add Avatars

--- packages/cwds-components/src/Avatar/Avatar.jsx
+++ packages/cwds-components/src/Avatar/Avatar.jsx
@@ -7,18 +7,32 @@ import styles from './Avatar.module.scss';
 const propTypes = {
   imgUrl: PropTypes.string,
   size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+  text: PropTypes.string,
+  invert: PropTypes.bool,
 };
 
 const defaultProps = {
   size: 'md',
 };
 
-const Avatar = ({ imgUrl, size }) => {
-  const classes = cn(styles.avatar, size && `avatar-${size}`);
-  return imgUrl ? (
-    <img src={imgUrl} className={classes} />
-  ) : (
-    <Icon icon="user" size="2x" className={classes} />
+const Avatar = ({ imgUrl, size, text, invert }) => {
+  const classes = cn(styles.avatar, styles[`size-${size}`], {
+    [styles.inverted]: invert,
+  });
+  if (imgUrl) {
+    return <img src={imgUrl} className={classes} />;
+  }
+  if (text) {
+    return (
+      <div className={classes}>
+        <div className={cn(styles.avatarInitials)}>{text}</div>
+      </div>
+    );
+  }
+  return (
+    <div className={classes}>
+      <Icon icon="user" size="2x" className={cn(styles.avatarInitials)} />
+    </div>
   );
 };
 
