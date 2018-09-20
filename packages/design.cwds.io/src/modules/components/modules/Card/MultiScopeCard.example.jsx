deb1321cd16974d1ae6d8fe6e05fab3ca8c6fb70
Danny Eck <danny.eck@osi.ca.gov>
2018-09-20 14:34:56 -0700

    Use Modal component instead of confirm

--- packages/design.cwds.io/src/modules/components/modules/Card/MultiScopeCard.example.jsx
+++ packages/design.cwds.io/src/modules/components/modules/Card/MultiScopeCard.example.jsx
@@ -9,6 +9,7 @@ import Label from '@cwds/components/lib/Label';
 import Input from '@cwds/components/lib/Input';
 import Icon from '@cwds/components/lib/Icon';
 import Alert from '@cwds/components/lib/Alert';
+import Modal from '@cwds/components/lib/Modal';
 
 class Example extends Component {
   state = {
@@ -20,6 +21,7 @@ class Example extends Component {
       animal: 'men',
     },
     isSaved: false,
+    needsConfirmation: false,
   };
 
   handleSubmit = () => {
@@ -27,6 +29,14 @@ class Example extends Component {
       setTimeout(() => resolve(), 1000);
     });
     return promise;
+  };
+
+  handleConfirmationDiscardOk = cb => {
+    this.setState({ needsConfirmation: false }, cb());
+  };
+
+  handleConfirmationDiscardCancel = () => {
+    this.setState({ needsConfirmation: false });
   };
 
   handleCancel = () => {
@@ -83,114 +93,150 @@ class Example extends Component {
               const onChange = e =>
                 props.setFieldValue(e.target.name, e.target.value);
               return (
-                <form onSubmit={props.handleSubmit} autoComplete="off">
-                  <Card>
-                    {renderHeader({ scope })}
-                    <Card.Body>
-                      {this.state.isSaved && (
-                        <Alert color="success">Yay! It worked.</Alert>
-                      )}
-                      <Row>
-                        <Col md={4}>
-                          <FormGroup>
-                            <Label htmlFor="multiscopedemo__timeUnit">
-                              Time Unit (plural)
-                            </Label>
-                            <Input
-                              id="multiscopedemo__timeUnit"
-                              name="timeUnit"
-                              type="text"
-                              onChange={onChange}
-                              value={props.values.timeUnit}
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col md={4}>
-                          <FormGroup>
-                            <Label htmlFor="multiscopedemo__relative">
-                              Relative (plural)
-                            </Label>
-                            <Input
-                              id="multiscopedemo__relative"
-                              name="relative"
-                              type="text"
-                              onChange={onChange}
-                              value={props.values.relative}
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col md={4}>
-                          <FormGroup>
-                            <Label htmlFor="multiscopedemo__verbPast">
-                              Verb (past tense)
-                            </Label>
-                            <Input
-                              id="multiscopedemo__verbPast"
-                              name="verbPast"
-                              type="text"
-                              onChange={onChange}
-                              value={props.values.verbPast}
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col md={4}>
-                          <FormGroup>
-                            <Label htmlFor="multiscopedemo__animal">
-                              Animal (plural)
-                            </Label>
-                            <Input
-                              id="multiscopedemo__animal"
-                              name="animal"
-                              type="text"
-                              onChange={onChange}
-                              value={props.values.animal}
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                    </Card.Body>
-                    <Card.Footer>
-                      <Button
-                        disabled={props.isSubmitting}
-                        onClick={() => {
-                          const { initialValues, values, dirty } = props;
-                          if (dirty) {
-                            const discardChanges = window.confirm(
-                              'Oh No! You may lose work. Discard changes?'
-                            );
-                            if (discardChanges) {
-                              props.resetForm();
-                              toggleScope();
+                <Fragment>
+                  <form onSubmit={props.handleSubmit} autoComplete="off">
+                    <Card>
+                      {renderHeader({ scope })}
+                      <Card.Body>
+                        {this.state.isSaved && (
+                          <Alert color="success">Hooray! Changes saved.</Alert>
+                        )}
+                        <Row>
+                          <Col md={4}>
+                            <FormGroup>
+                              <Label htmlFor="multiscopedemo__timeUnit">
+                                Time Unit (plural)
+                              </Label>
+                              <Input
+                                id="multiscopedemo__timeUnit"
+                                name="timeUnit"
+                                type="text"
+                                onChange={onChange}
+                                value={props.values.timeUnit}
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col md={4}>
+                            <FormGroup>
+                              <Label htmlFor="multiscopedemo__relative">
+                                Relative (plural)
+                              </Label>
+                              <Input
+                                id="multiscopedemo__relative"
+                                name="relative"
+                                type="text"
+                                onChange={onChange}
+                                value={props.values.relative}
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col md={4}>
+                            <FormGroup>
+                              <Label htmlFor="multiscopedemo__verbPast">
+                                Verb (past tense)
+                              </Label>
+                              <Input
+                                id="multiscopedemo__verbPast"
+                                name="verbPast"
+                                type="text"
+                                onChange={onChange}
+                                value={props.values.verbPast}
+                              />
+                            </FormGroup>
+                          </Col>
+                          <Col md={4}>
+                            <FormGroup>
+                              <Label htmlFor="multiscopedemo__animal">
+                                Animal (plural)
+                              </Label>
+                              <Input
+                                id="multiscopedemo__animal"
+                                name="animal"
+                                type="text"
+                                onChange={onChange}
+                                value={props.values.animal}
+                              />
+                            </FormGroup>
+                          </Col>
+                        </Row>
+                      </Card.Body>
+                      <Card.Footer>
+                        <Button
+                          disabled={props.isSubmitting}
+                          onClick={() => {
+                            const { initialValues, values, dirty } = props;
+                            if (dirty) {
+                              // const discardChanges = window.confirm(
+                              //   'Oh No! You may lose work. Discard changes?'
+                              // );
+                              // this.setState({
+                              //   confirmationDialog: new Promise((resolve, reject) => {
+
+                              //   })
+                              // });
+                              this.setState({
+                                needsConfirmation: true,
+                              });
+                              // if (discardChanges) {
+                              //   props.resetForm();
+                              //   toggleScope();
+                              // } else {
+                              //   // do nothing
+                              // }
                             } else {
-                              // do nothing
+                              this.setState({ isSaved: false });
+                              toggleScope();
                             }
-                          } else {
-                            this.setState({ isSaved: false });
-                            toggleScope();
+                          }}
+                        >
+                          Go Back
+                        </Button>{' '}
+                        <Button
+                          type="submit"
+                          color="primary"
+                          disabled={
+                            !(props.dirty && props.isValid) ||
+                            props.isSubmitting
                           }
-                        }}
+                        >
+                          {props.isSubmitting ? (
+                            <Fragment>
+                              <Icon className="mr-2" icon="circleNotch" spin />
+                              Saving...
+                            </Fragment>
+                          ) : (
+                            'Save'
+                          )}
+                        </Button>
+                      </Card.Footer>
+                    </Card>
+                  </form>
+                  <Modal isOpen={this.state.needsConfirmation}>
+                    <Modal.Header>Oops!</Modal.Header>
+                    <Modal.Body>
+                      Do you want to discard your unsaved changes?
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button
+                        color="secondary"
+                        onClick={this.handleConfirmationDiscardCancel}
                       >
-                        Go Back
+                        Edit
                       </Button>{' '}
                       <Button
-                        type="submit"
-                        color="primary"
-                        disabled={
-                          !(props.dirty && props.isValid) || props.isSubmitting
+                        color="warning"
+                        onClick={() =>
+                          this.handleConfirmationDiscardOk(() => {
+                            props.resetForm();
+                            toggleScope();
+                          })
                         }
                       >
-                        {props.isSubmitting ? (
-                          <Fragment>
-                            <Icon className="mr-2" icon="circleNotch" spin />
-                            Saving...
-                          </Fragment>
-                        ) : (
-                          'Save'
-                        )}
+                        Discard
                       </Button>
-                    </Card.Footer>
-                  </Card>
-                </form>
+                    </Modal.Footer>
+                  </Modal>
+                </Fragment>
               );
             }}
           />
