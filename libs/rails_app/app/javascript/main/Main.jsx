1bfb0e4d9f4c9e9b0168d619749b314b92722a6d
Danny Eck <danny.eck@osi.ca.gov>
2018-10-11 13:34:38 -0700

    WiP

---
+++ libs/rails_app/app/javascript/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = props => (
+  <div>
+    <h3>Hello World</h3>
+  </div>
+);
+
+export default Main;
--- libs/rails_app/config/routes.rb
+++ libs/rails_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index'
+
+  # get '*path', to: 'main#index', constraints: ->(request) do
+  #   !request.xhr? && request.format.html?
+  # end
 end
