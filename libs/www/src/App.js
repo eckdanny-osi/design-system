df448d586901f0b96a58551395e6de80d560d25e
Danny Eck <danny.eck@osi.ca.gov>
2018-10-10 17:35:41 -0700

    Fix MDXComponentProvider

--- libs/www/src/App.js
+++ libs/www/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import { MDXProvider } from '@mdx-js/tag';
-// import '@test/components/dist/global/styles.scss';
+import { MDXProvider } from '@mdx-js/tag';
 import '@cwds/core/dist/styles.css';
 // import 'react-select/dist/react-select.css';
 
 import JumpToTop from '@cwds/components/dist/JumpToTop';
-// import componentMap from './components/MDXComponentProviderMappings';
+import componentMap from './components/MDXComponentProviderMappings';
 
 import ScrollToTop from './ScrollToTop';
 // import Status from './modules/status';
@@ -21,23 +20,23 @@ import CoreStyles from './modules/core-styles';
 class App extends Component {
   render() {
     return (
-      // <MDXProvider components={componentMap}>
-      <Router>
-        <ScrollToTop>
-          <Fragment>
-            <Route exact path="/" component={Home} />
-            {/* <Route path="/page-layouts" component={PageLayouts} /> */}
-            <Route path="/core-style" component={CoreStyles} />
-            {/* <Route path="/principles" component={PrinciplesPage} /> */}
-            {/* <Route path="/labs" component={LabsPage} /> */}
-            {/* <Route path="/components-prev" component={ComponentBrowser} /> */}
-            {/* <Route path="/components" component={Components} /> */}
-            {/* <Route path="/status" component={Status} /> */}
-            <JumpToTop />
-          </Fragment>
-        </ScrollToTop>
-      </Router>
-      // </MDXProvider>
+      <MDXProvider components={componentMap}>
+        <Router>
+          <ScrollToTop>
+            <Fragment>
+              <Route exact path="/" component={Home} />
+              {/* <Route path="/page-layouts" component={PageLayouts} /> */}
+              <Route path="/core-style" component={CoreStyles} />
+              {/* <Route path="/principles" component={PrinciplesPage} /> */}
+              {/* <Route path="/labs" component={LabsPage} /> */}
+              {/* <Route path="/components-prev" component={ComponentBrowser} /> */}
+              {/* <Route path="/components" component={Components} /> */}
+              {/* <Route path="/status" component={Status} /> */}
+              <JumpToTop />
+            </Fragment>
+          </ScrollToTop>
+        </Router>
+      </MDXProvider>
     );
   }
 }
