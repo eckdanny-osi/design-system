0f286e7932881aed36edb4297b85ea7d94b9df73
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2018-10-15 14:14:00 -0700

    Resolve broken build (#144)
    * WiP
    
    * Remove sneaky dep on react-router
    
    * Update yarn lock

--- libs/components/src/BreadcrumbTrail/BreadcrumbTrail.jsx
+++ libs/components/src/BreadcrumbTrail/BreadcrumbTrail.jsx
@@ -5,21 +5,24 @@ import { Container } from '../Grid';
 import Styles from './BreadcrumbTrail.module.scss';
 
 // @TODO: this should break!!! Only works because of lerna!
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+const NavLink = props => <a href={props.to}>{props.children}</a>;
 
 const BreadcrumbTrail = ({ items = [], renderItem }) => {
   return (
     <div className={cn(Styles.BreadcrumbTrail)}>
       <Breadcrumb className="pt-0" style={{ width: 'max-content' }}>
         {items.map((item, i) => (
-          <Breadcrumb.Item
-            key={i}
-            active={i + 1 === items.length ? true : false}
-            tag={NavLink}
-            to={item.path}
-          >
-            {item.title}
-          </Breadcrumb.Item>
+          <React.Fragment key={i}>
+            {i !== 0 && <div className="mx-2 text-primary">/</div>}
+            <Breadcrumb.Item
+              active={i + 1 === items.length ? true : false}
+              tag={NavLink}
+              to={item.path}
+            >
+              {item.title}
+            </Breadcrumb.Item>
+          </React.Fragment>
         ))}
       </Breadcrumb>
     </div>
