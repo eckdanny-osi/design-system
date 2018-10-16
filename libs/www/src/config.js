d0f5ad5094592a4ec37b97f9e89416767e228310
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2018-10-16 00:03:00 -0700

    Fix status page babel macros (#150)
    * use babel-macros
    
    * Moar info
    
    * typos easy to miss when prog in tmpl literals
    
    * Add build vars

--- libs/www/src/config.js
+++ libs/www/src/config.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import AppBar from '@cwds/components/dist/AppBar';
 import Logo from '@cwds/components/dist/Logo';
 import Badge from '@cwds/components/dist/Badge';
-// import status from './modules/status/status';
+import status from './modules/status/status';
 
 // const libVersion = status.lerna.find(d => d.name === '@cwds/components')
 //   .version;
@@ -19,7 +19,7 @@ AppBar.config({
       </Link>
       <Link to="/status" className="d-none d-sm-inline-block">
         <Badge color="primary" className="ml-2">
-          {'version?'}
+          {status.version}
         </Badge>
       </Link>
     </React.Fragment>
--- libs/www/src/modules/status/StatusPage.jsx
+++ libs/www/src/modules/status/StatusPage.jsx
@@ -1,100 +1,131 @@
 import React from 'react';
+import slugify from 'slugify';
+import { Link } from 'react-router-dom';
 import { Row, Col } from '@cwds/components/dist/Grid';
 import Card from '@cwds/components/dist/Cards';
 import Page from '@cwds/components/dist/Layouts/Page';
 import Button from '@cwds/components/dist/Button';
 import slackLogo from './Slack_Mark_White_Web.png';
 import githubLogo from './GitHub-Mark-Light-64px.png';
-// import status from './status';
 import Style from './StatusButton.module.css';
+import status from './status';
 
-const status = {
-  lerna: [],
-  pkg: {
-    bugs: {
-      url: '',
-    },
-  },
-};
+import JumpNav from '@cwds/components/dist/JumpNav';
+
+const toSlug = str =>
+  slugify(str, {
+    replacement: '-',
+    lower: true,
+  });
+
+const routes = ['Report an Issue', 'Build Info', 'Packages'].map(title => ({
+  title,
+  path: `#${toSlug(title)}`,
+}));
 
 export default () => (
   <Page
     title="Status"
-    layout="dashboard"
+    layout="jumpnav"
     breadcrumb={[
       { path: '/', title: 'Home' },
       { path: '/status', title: 'Status' },
     ]}
+    sidenav={() => {
+      return <JumpNav tag={Link} routes={routes} />;
+    }}
     main={() => (
       <Row>
         <Col>
+          <h2 id="report-an-issue">
+            <a href="#report-an-issue">Report an Issue</a>
+          </h2>
           <Card>
-            <Card.Header>
-              <Card.Title>Build Info</Card.Title>
-            </Card.Header>
-            <Card.Body>
-              <table style={{ width: '100%' }}>
-                <tbody>
-                  <tr>
-                    <td>Build Date</td>
-                    <td>
-                      <tt>{status.buildDate}</tt>
-                    </td>
-                  </tr>
-                  {status.lerna.map(({ name, version }) => {
-                    return (
-                      <tr key={name}>
-                        <td>
-                          <tt>{name}</tt>
-                        </td>
-                        <td>
-                          <tt>{version}</tt>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                  <tr>
-                    <td>Branch</td>
-                    <td>
-                      <tt>{status.gitInfo}</tt>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </Card.Body>
+            <Button
+              color="primary"
+              href="slack://channel?team=T0FSW5RLH&id=C34SC4BMF"
+              className={Style.StatusButton}
+            >
+              <img
+                src={slackLogo}
+                className={Style.BlockLogo}
+                alt="slack logo"
+              />
+              #design-ops
+            </Button>{' '}
+            <Button
+              color="primary"
+              href={status.main.bugs.url}
+              className={Style.StatusButton}
+              target="_blank"
+            >
+              <img
+                src={githubLogo}
+                className={Style.BlockLogo}
+                alt="github logo"
+              />
+              Issues
+            </Button>
           </Card>
+
+          <h2 id="build-info">
+            <a href="#build-info">Build Info</a>
+          </h2>
           <Card>
-            <Card.Header>
-              <Card.Title>Issues</Card.Title>
-            </Card.Header>
-            <Card.Body>
-              <Button
-                color="primary"
-                href="slack://channel?team=T0FSW5RLH&id=C34SC4BMF"
-                className={Style.StatusButton}
-              >
-                <img
-                  src={slackLogo}
-                  className={Style.BlockLogo}
-                  alt="slack logo"
-                />
-                #design-ops
-              </Button>{' '}
-              <Button
-                color="primary"
-                href={status.pkg.bugs.url}
-                className={Style.StatusButton}
-                target="_blank"
-              >
-                <img
-                  src={githubLogo}
-                  className={Style.BlockLogo}
-                  alt="github logo"
-                />
-                Issues
-              </Button>{' '}
-            </Card.Body>
+            <table style={{ width: '100%' }}>
+              <tbody>
+                <tr>
+                  <td>Build Date</td>
+                  <td>
+                    <tt>{status.buildDate}</tt>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Branch</td>
+                  <td>
+                    <tt>{status.git}</tt>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Info</td>
+                  <td>
+                    <pre>
+                      <code>{JSON.stringify(status.build, null, 2)}</code>
+                    </pre>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </Card>
+          <h2 id="packages">
+            <a href="#packages">Packages</a>
+          </h2>
+          {status.packages.map(pkg => {
+            return (
+              <Card key={pkg.name}>
+                <Card.Header>
+                  <h3
+                    className="m-0"
+                    style={{
+                      fontFamily:
+                        'SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+                      fontSize: '85%',
+                    }}
+                  >
+                    {pkg.name}
+                  </h3>
+                </Card.Header>
+                <Card.Body>
+                  <h4>Dependencies</h4>
+                  <pre className="mb-0">
+                    <code>
+                      {JSON.stringify(pkg.pkgJson.dependencies, null, 2)}
+                    </code>
+                  </pre>
+                </Card.Body>
+              </Card>
+            );
+          })}
         </Col>
       </Row>
     )}
