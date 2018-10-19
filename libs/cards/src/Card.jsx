9d282d40b55eb1a169b47fb7f7aac86210028eb8
Danny Eck <danny.eck@osi.ca.gov>
2018-10-18 22:46:11 -0700

    WiP

--- libs/cards/src/Card.jsx
+++ libs/cards/src/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Card as CardUnstyled } from 'reactstrap';
-// import Styles from './Cards.module.scss';
+import Styles from './Cards.module.scss';
 
 const Card = props => <CardUnstyled {...props} />;
 Card.propTypes = { ...CardUnstyled.propTypes };
-Card.defaultProps = { ...Card.defaultProps, cssModule: {} };
+Card.defaultProps = { ...Card.defaultProps, cssModule: Styles };
 
 export default Card;
