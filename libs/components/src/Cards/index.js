253f33863d29316a59733985762ea3eb425dcd31
Danny Eck <danny.eck@osi.ca.gov>
2018-10-24 16:55:33 -0700

    STILL DIRTY

--- libs/components/src/Cards/index.js
+++ libs/components/src/Cards/index.js
@@ -1,8 +1,15 @@
 // export { Card, CardUnstyled } from './Card';
-export { default as CardBody } from './CardBody'
-export { default as CardFooter } from './CardFooter'
-export { default as CardHeader } from './CardHeader'
-export { default as CardSubtitle } from './CardSubtitle'
-export { default as CardDeck } from './CardDeck'
+// export { default as CardBody } from './CardBody'
+// export { default as CardFooter } from './CardFooter'
+// export { default as CardHeader } from './CardHeader'
+// export { default as CardSubtitle } from './CardSubtitle'
+// export { default as CardDeck } from './CardDeck'
 
+export {
+  CardBody,
+  CardHeader,
+  CardFooter,
+  CardSubtitle,
+  CardDeck,
+} from 'reactstrap'
 export { default } from './Card'
