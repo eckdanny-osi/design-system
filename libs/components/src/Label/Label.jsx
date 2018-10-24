262f0d0955cf9cc55672730235c95e32b4a29b55
Danny Eck <danny.eck@osi.ca.gov>
2018-10-24 13:42:10 -0700

    Remove Label

--- libs/components/src/Label/Label.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import { Label as LabelUnstyled } from 'reactstrap'
-import Styles from '../Forms//Forms.module.scss'
-
-const Label = props => <LabelUnstyled {...props} />
-Label.propTypes = { ...LabelUnstyled.propTypes }
-Label.defaultProps = { ...LabelUnstyled.defaultProps, cssModule: Styles }
-
-export default Label
