7403e9922102d898654ca53b0f6db19e7475dbc8
Danny Eck <danny.eck@osi.ca.gov>
2018-10-24 12:06:27 -0700

    Good

--- libs/core/js/bridge.js
+++ libs/core/js/bridge.js
@@ -1,20 +1,22 @@
-import DS, { getSpacers, getBreakpoints } from './core'
+import DS from './core'
 
-const breakpoints = Object.keys(DS.breakpoints)
-  .map(key => ({ key, value: DS.breakpoints[key] }))
-  .reduce((acc, kv) => {
-    return {
-      ...acc,
-      [kv.key]: `${kv.value}px`,
-    }
-  }, {})
+const toSass = (map, keyPrefix = '', unit = false) => {
+  return Object.keys(map)
+    .map(k => ({ key: k, value: map[k] }))
+    .reduce((acc, { key, value }, i) => {
+      return {
+        ...acc,
+        [`${keyPrefix}${key}`]: unit ? `${value}${unit}` : value,
+      }
+    }, {})
+}
 
 export default {
   ...DS.colors,
-  ...DS.grays,
-  ...DS.themeColors,
-  spacer: `${DS.spacer}rem`,
-  ...DS.spacers,
-  ...breakpoints,
+  ...toSass(DS.grays, 'gray-'),
+  ...toSass(DS.themeColors),
+  spacer: `${DS.spacing.spacer}${DS.spacing.spacerUnit}`,
+  ...toSass(DS.spacing.spacers, 'spacer-', DS.spacing.spacerUnit),
+  ...toSass(DS.breakpoints, 'grid-breakpoint-', DS.breakpointUnit),
   ...DS.options,
 }
