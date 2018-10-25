e2e98b53ab1fa001435ba1cfeeacc1cea090c0be
Danny Eck <danny.eck@osi.ca.gov>
2018-10-24 21:37:52 -0700

    Cleaned up

--- libs/components/src/index.js
+++ libs/components/src/index.js
@@ -1,3 +1,4 @@
+import * as Utils from './utils'
 export { Alert, AlertUncontrolled } from './Alert'
 export { default as AppBar } from './AppBar'
 export { default as Avatar } from './Avatar'
@@ -5,13 +6,68 @@ export { default as BreadcrumbTrail } from './BreadcrumbTrail'
 export { default as CheckboxBank } from './CheckboxBank'
 export { default as DataGrid } from './DataGrid'
 export { default as Field } from './Field'
+export { default as Icon, ICON_NAMES } from '@cwds/icons'
+export { default as InfoTip } from './InfoTip'
+export { default as JumpNav } from './JumpNav'
+export { default as JumpToTop } from './JumpToTop'
+export { default as Layout, LayoutJumpNav, LayoutSimple, Page } from './Layouts'
+export { default as Legend } from './Legend'
+export { default as Logo } from './Logo'
+export { default as PageHeader } from './PageHeader'
+export { default as RadioGroup } from './RadioGroup'
+export { default as Select } from './Select'
+export { Utils }
 
+/**
+ * Reactstrap Components
+ *
+ * Omit:
+ *   - Alert
+ *   - CardBlock
+ *   - CardColumns
+ *   - CardDeck
+ *   - CardGroup
+ *   - CardImg
+ *   - CardImgOverlay
+ *   - CardLink
+ *   - CardSubtitle
+ *   - CardText
+ *   - Carousel
+ *   - CarouselCaption
+ *   - CarouselControl
+ *   - CarouselIndicators
+ *   - CarouselItem
+ *   - CustomInput
+ *   - InputGroupButton
+ *   - InputGroupButtonDropdown
+ *   - InputGroupText
+ *   - Jumbotron
+ *   - Media
+ *   - Navbar
+ *   - NavbarBrand
+ *   - NavbarToggler
+ *   - NavDropdown
+ *   - Pagination
+ *   - PaginationItem
+ *   - PaginationLink
+ *   - PopperContent
+ *   - PopperTargetHelper
+ *   - Progress
+ *   - TabContent
+ *   - Table
+ *   - TabPane
+ *   - UncontrolledCarousel
+ *   - UncontrolledCollapse
+ *   - UncontrolledNavDropdown
+ */
 export {
   Badge,
   Breadcrumb,
   BreadcrumbItem,
   Button,
+  ButtonDropdown,
   ButtonGroup,
+  ButtonToolbar,
   Card,
   CardBody,
   CardFooter,
@@ -24,15 +80,19 @@ export {
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
+  Fade,
   Form,
   FormFeedback,
   FormGroup,
+  FormText,
   Input,
   InputGroup,
   InputGroupAddon,
   Label,
   ListGroup,
   ListGroupItem,
+  ListGroupItemHeading,
+  ListGroupItemText,
   Modal,
   ModalBody,
   ModalFooter,
@@ -47,110 +107,11 @@ export {
   PopoverTitle,
   Row,
   Tooltip,
+  UncontrolledAlert,
+  UncontrolledButtonDropdown,
+  UncontrolledDropdown,
+  UncontrolledTooltip,
+  Util,
 } from 'reactstrap'
-export { default as Icon, ICON_NAMES } from '@cwds/icons'
-export { default as InfoTip } from './InfoTip'
-export { default as JumpNav } from './JumpNav'
-export { default as JumpToTop } from './JumpToTop'
-export { default as Layout, LayoutJumpNav, LayoutSimple, Page } from './Layouts'
-export { default as Legend } from './Legend'
-export { default as Logo } from './Logo'
-export { default as PageHeader } from './PageHeader'
-
-export { default as RadioGroup } from './RadioGroup'
-export { default as Select } from './Select'
-import * as Utils from './utils'
-export { Utils }
-
-// export {
-//   Alert,
-//   Container,
-//   Row,
-//   Col,
-//   Navbar,
-//   NavbarBrand,
-//   NavbarToggler,
-//   Nav,
-//   NavItem,
-//   NavDropdown,
-//   NavLink,
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   Button,
-//   ButtonDropdown,
-//   ButtonGroup,
-//   ButtonToolbar,
-//   Dropdown,
-//   DropdownItem,
-//   DropdownMenu,
-//   DropdownToggle,
-//   Fade,
-//   Badge,
-//   Card,
-//   CardLink,
-//   CardGroup,
-//   CardDeck,
-//   CardColumns,
-//   CardBody,
-//   CardBlock,
-//   CardFooter,
-//   CardHeader,
-//   CardImg,
-//   CardImgOverlay,
-//   Carousel,
-//   UncontrolledCarousel,
-//   CarouselControl,
-//   CarouselItem,
-//   CarouselIndicators,
-//   CarouselCaption,
-//   CardSubtitle,
-//   CardText,
-//   CardTitle,
-//   Popover,
-//   PopoverContent,
-//   PopoverBody,
-//   PopoverTitle,
-//   PopoverHeader,
-//   Progress,
-//   Modal,
-//   ModalHeader,
-//   ModalBody,
-//   ModalFooter,
-//   PopperContent,
-//   PopperTargetHelper,
-//   Tooltip,
-//   Table,
-//   ListGroup,
-//   Form,
-//   FormFeedback,
-//   FormGroup,
-//   FormText,
-//   Input,
-//   InputGroup,
-//   InputGroupAddon,
-//   InputGroupButton,
-//   InputGroupButtonDropdown,
-//   InputGroupText,
-//   Label,
-//   CustomInput,
-//   Media,
-//   Pagination,
-//   PaginationItem,
-//   PaginationLink,
-//   TabContent,
-//   TabPane,
-//   Jumbotron,
-//   Collapse,
-//   ListGroupItem,
-//   ListGroupItemText,
-//   ListGroupItemHeading,
-//   UncontrolledAlert,
-//   UncontrolledButtonDropdown,
-//   UncontrolledCollapse,
-//   UncontrolledDropdown,
-//   UncontrolledNavDropdown,
-//   UncontrolledTooltip,
-//   Util,
-// } from 'reactstrap'
 
 import './init'
