80b5a42ffcea94380dd567089c0599ac996e7600
Danny Eck <danny.eck@osi.ca.gov>
2018-10-24 17:20:30 -0700

    Remove BUttons

--- libs/components/src/Buttons/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import cn from 'classnames'
-import { Button as ButtonUnstyled } from 'reactstrap'
-import Styles from '../Buttons.module.scss'
-
-const Button = props => <ButtonUnstyled {...props} />
-Button.defaultProps = { ...ButtonUnstyled.defaultProps }
-
-export default Button
