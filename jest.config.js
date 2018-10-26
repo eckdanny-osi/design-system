892d39b5244dc0687685167858db90441c1661ba
Danny Eck <danny.eck@osi.ca.gov>
2018-10-26 11:09:30 -0700

    Workspace run test only on libs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   verbose: true,
-  projects: ['<rootDir>/libs/*'],
+  projects: ['<rootDir>/libs/!(rails_app|www)'],
   setupTestFrameworkScriptFile: '<rootDir>/setupTests.js',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
