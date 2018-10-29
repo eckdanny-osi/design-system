fad91a00c320ef6809c9bb258029c5704889a383
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2018-10-29 11:10:46 -0700

    (fix): delete the offending col class (#208)

--- libs/reactstrap/src/init.js
+++ libs/reactstrap/src/init.js
@@ -1,4 +1,7 @@
 import { Util } from 'reactstrap'
 import cssModule from '@cwds/core/dist/bootstrap-cares.module.css'
 
+// I am not sure why, but a `col` class is created. Must remove it in order to allow the grid globals to fall thru. See https://github.com/ca-cwds/design-system/issues/207
+delete cssModule.col
+
 Util.setGlobalCssModule(cssModule)
