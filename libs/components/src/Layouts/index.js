f3af1b8bf0be034c217a67a9b26dda76d59172ed
Danny Eck <danny.eck@osi.ca.gov>
2018-11-20 10:53:51 -0800

    Remove unused files

--- libs/components/src/Layouts/index.js
+++ libs/components/src/Layouts/index.js
@@ -1,4 +1,1 @@
-export { default } from './Layout'
-export { default as LayoutSimple } from './LayoutSimple'
-export { default as LayoutJumpNav } from './LayoutJumpNav'
 export { default as Page } from './Page'
