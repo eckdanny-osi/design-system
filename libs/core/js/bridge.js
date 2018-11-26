394e1cc07de7baf5aeafcd8cc21c70d040d84b8b
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2018-11-26 11:06:25 -0800

    fix(core): Use preval to ship plain object DS config (#257)

--- libs/core/js/bridge.js
+++ libs/core/js/bridge.js
@@ -1,17 +1,38 @@
-import DS from './core'
+// @preval
 
-const toSass = (map, keyPrefix = '', unit = false) => {
+/**
+ * Bridge to Sass Vars
+ *
+ * STOP! This file compiles to a simple object export of the Bootstrap 4 sass
+ * variables ready for importing with our LibSass importer. If you want to
+ * declare new design system directives use `core.js`.
+ *
+ * We are using `preval` to evaluate config values at build-time and distribute
+ * the Design System (DS) config as simple JSON. This means node-safe syntax
+ * only (no esm imports).
+ */
+
+const DS = require('./core')
+
+/**
+ * Helper to create Sass variables from JS Objects
+ * @param {Object} map
+ * @param {string} keyPrefix
+ * @param {boolean} unit
+ */
+function toSass(map, keyPrefix = '', unit = false) {
   return Object.keys(map)
     .map(k => ({ key: k, value: map[k] }))
-    .reduce((acc, { key, value }, i) => {
-      return {
+    .reduce(
+      (acc, { key, value }, i) => ({
         ...acc,
         [`${keyPrefix}${key}`]: unit ? `${value}${unit}` : value,
-      }
-    }, {})
+      }),
+      {}
+    )
 }
 
-export default {
+module.exports = {
   ...DS.colors,
   ...toSass(DS.grays, 'gray-'),
   ...toSass(DS.themeColors),
--- libs/core/js/core.js
+++ libs/core/js/core.js
@@ -1,6 +1,26 @@
-import Color from 'color'
+// @preval
 
+/**
+ * Design System
+ *
+ * This is the source of truth for design system variable declarations. It is
+ * distributed as a javascript module so it may be used in downstream processes
+ * such as css-in-js component theming or in sass systems such as Bootstrap 4
+ * using LibSass's importer feature.
+ *
+ * This file is organized consistent with BS _variables.scss.
+ *
+ * We are using `preval` to evaluate config values at build-time and distribute
+ * the Design System (DS) config as simple JSON. This means node-safe syntax
+ * only (no esm imports).
+ */
+
+const Color = require('color')
 const DS = {}
+
+//
+// Color system
+//
 
 // Colors
 DS.colors = {
@@ -10,42 +30,48 @@ DS.colors = {
 
 // Grays
 DS.grays = {
-  '100': Color.hsl(0, 0, 96).hex(), // ($ray)
-  '200': Color.hsl(0, 0, 91).hex(), // ($grouper)
-  '300': Color.hsl(0, 0, 82).hex(), // ($trout)
-  '400': Color.hsl(0, 0, 60).hex(), //
-  '500': Color.hsl(0, 0, 50).hex(), //
-  '600': Color.hsl(0, 0, 40).hex(), //
-  '700': Color.hsl(0, 0, 36).hex(), // ($wahoo)
-  '800': Color.hsl(0, 0, 20).hex(), //
-  '900': Color.hsl(0, 0, 10).hex(), // ($shark)
+  '100':        Color.hsl(0, 0, 96).hex(),   // ($ray)
+  '200':        Color.hsl(0, 0, 91).hex(),   // ($grouper)
+  '300':        Color.hsl(0, 0, 82).hex(),   // ($trout)
+  '400':        Color.hsl(0, 0, 60).hex(),   //
+  '500':        Color.hsl(0, 0, 50).hex(),   //
+  '600':        Color.hsl(0, 0, 40).hex(),   //
+  '700':        Color.hsl(0, 0, 36).hex(),   // ($wahoo)
+  '800':        Color.hsl(0, 0, 20).hex(),   //
+  '900':        Color.hsl(0, 0, 10).hex(),   // ($shark)
 }
 
 // Theme Colors
 DS.themeColors = {
-  primary: Color('#09798e').hex(), // ($aspen)
-  secondary: DS.colors['white'],
-  accent: Color('#601974').hex(), // ($oak)
-  success: Color('#068347').hex(), // ($elm)
-  info: Color('#0d4f78').hex(), // ($spruce)
-  warning: Color('#e7740b').hex(), // ($hickory)
-  danger: Color('#bb3709').hex(), // ($maple)
-  light: DS.grays['100'], // '$gray-100',
-  dark: DS.grays['800'], // '$gray-800',
+  primary:      Color('#09798e').hex(),   // ($aspen)
+  secondary:    DS.colors['white'],       // $white
+  accent:       Color('#601974').hex(),   // ($oak)
+  success:      Color('#068347').hex(),   // ($elm)
+  info:         Color('#0d4f78').hex(),   // ($spruce)
+  warning:      Color('#e7740b').hex(),   // ($hickory)
+  danger:       Color('#bb3709').hex(),   // ($maple)
+  light:        DS.grays['100'],          // '$gray-100',
+  dark:         DS.grays['800'],          // '$gray-800',
 }
 
+//
 // Options
+//
+
 DS.options = {
-  // 'enable-caret': true,
-  // 'enable-rounded': true,
-  'enable-shadows': true,
-  // 'enable-gradients': false,
-  // 'enable-transitions': true,
-  // 'enable-grid-classes': true,
-  // 'enable-print-styles': true,
+  // 'enable-caret':           true,
+  // 'enable-rounded':         true,
+  'enable-shadows':            true,
+  // 'enable-gradients':       false,
+  // 'enable-transitions':     true,
+  // 'enable-grid-classes':    true,
+  // 'enable-print-styles':    true,
 }
 
+//
 // Spacing
+//
+
 DS.spacing = {}
 DS.spacing.spacerUnit = 'rem'
 DS.spacing.spacer = 1 // rem
@@ -58,7 +84,10 @@ DS.spacing.spacers = {
   '5': DS.spacing.spacer * 3,
 }
 
-// Breakpoints
+//
+// Grid breakpoints
+//
+
 DS.breakpointUnit = 'px'
 DS.breakpoints = {
   xs: 0,
@@ -68,4 +97,4 @@ DS.breakpoints = {
   xl: 1200,
 }
 
-export default DS
+module.exports = DS
