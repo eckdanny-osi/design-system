c2dca2b1d012079b0b2a75f9c9dc6c7e2cf317d0
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2018-11-30 14:27:52 -0800

    Fix proptable readability (#275)
    * fix(www): improve PropTable docs-only component
    
    Use more document-oriented elems for props. Use anchors for
    deep-linkability, conditional chunks, transpose the reading direction so
    theres a lot more screen space for details
    
    * remove dead code

--- apps/www/src/components/PropTable/PropTable.jsx
+++ apps/www/src/components/PropTable/PropTable.jsx
@@ -1,59 +1,76 @@
-import React from 'react'
-import { Card, CardHeader, CardTitle, CardBody, Table } from '@cwds/components'
+import React, { Fragment } from 'react'
+import cn from 'classnames'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Table,
+  Badge,
+} from '@cwds/components'
+import AnchorLink from '../AnchorLink'
+import Styles from './PropTable.module.scss'
 
 const ResponsiveTag = props => (
   <div {...props} style={{ overflowX: 'scroll', marginTop: '-1px' }} />
 )
 
+const PropDoc = ({ propDef, tag: Tag = 'h4' }) => {
+  const { description, defaultValue, name, required, type } = propDef
+  return (
+    <div className={cn(Styles.PropDoc, 'py-3')}>
+      <AnchorLink tag={Tag} className="text-monospace" parentClassName="mb-0">
+        {name}
+      </AnchorLink>
+
+      <Table
+        className="mb-0 mt-2"
+        size="sm"
+        bordered
+        responsive
+        responsiveTag={ResponsiveTag}
+      >
+        <tbody>
+          <tr>
+            <td style={{ width: '20%' }}>Type</td>
+            <td>
+              <span className="text-monospace">{renderType(type)}</span>
+              {required && <Badge color="warning">required</Badge>}
+            </td>
+          </tr>
+          <tr>
+            <td>Description</td>
+            <td>{description || <span>None given</span>}</td>
+          </tr>
+          {defaultValue && (
+            <tr>
+              <td>Default</td>
+              <td>
+                <pre className="mb-0" style={{ verticalAlign: 'middle' }}>
+                  <code>{defaultValue.value}</code>
+                </pre>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </div>
+  )
+}
+
+const PropDocs = ({ docgen, headingLevel }) => (
+  <div className={Styles.PropDocs}>
+    {getPropsIterable(docgen.props).map(propDef => {
+      return <PropDoc key={propDef.name} propDef={propDef} />
+    })}
+  </div>
+)
+
 const PropTable = ({ docgen }) => {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>{docgen.displayName} PropTypes</CardTitle>
-      </CardHeader>
-      <CardBody className="pt-0">
-        <Table
-          className="mb-0"
-          size="sm"
-          responsive
-          responsiveTag={ResponsiveTag}
-        >
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Required</th>
-              <th>Default</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {getPropsIterable(docgen.props).map((prop, i) => {
-              const { description, name, required, defaultValue, type } = prop
-              return (
-                <tr key={i}>
-                  <td>
-                    <code className="text-info">{name}</code>
-                  </td>
-                  <td>
-                    <code className="text-dark">{renderType(type)}</code>
-                  </td>
-                  <td>
-                    <code className="text-dark">{required}</code>
-                  </td>
-                  <td>
-                    <code className="text-dark">
-                      {defaultValue && defaultValue.value}
-                    </code>
-                  </td>
-                  <td>
-                    <code className="text-dark">{description}</code>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
+      <CardBody className="py-0">
+        <PropDocs docgen={docgen} />
       </CardBody>
     </Card>
   )
