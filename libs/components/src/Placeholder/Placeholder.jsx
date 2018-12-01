e49c0ec4b7fe2f35b94bb2461c7db1af327c37b6
Danny Eck <danny.eck@osi.ca.gov>
2018-12-01 01:46:45 -0800

    iterate over rows and cols

--- libs/components/src/Placeholder/Placeholder.jsx
+++ libs/components/src/Placeholder/Placeholder.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Row, Col } from '@cwds/reactstrap'
 
-const Placeholder = () => {
-  return (
-    <div>
-      <h3>Placeholder</h3>
-    </div>
+// TODO: the `rows` and `cols` props should have the same reponsive prop format
+//       as the `Col` [size] interface (reactstrap)
+
+const ShimmerLine = () => <div>shimmer</div>
+
+const Placeholder = ({ cols, rows, tag: Tag }) => {
+  return cols > 1 ? (
+    <Row>
+      {new Array(cols).fill().map((_, i) => {
+        return (
+          <Col key={i}>
+            {new Array(rows).fill().map((_, j) => {
+              return <Tag key={j} />
+            })}
+          </Col>
+        )
+      })}
+    </Row>
+  ) : (
+    <span>alksdfj</span>
   )
+}
+Placeholder.propTypes = {
+  cols: PropTypes.number,
+  rows: PropTypes.number,
+  // TODO: what is the right tag here?
+  // tag: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+}
+Placeholder.defaultProps = {
+  cols: 1,
+  rows: 4,
+  tag: ShimmerLine,
 }
 
 export default Placeholder
+
+function indexArray(len) {
+  return new Array(len).fill().map((_, i) => i)
+}
--- libs/components/src/Placeholder/Placeholder.test.js
+++ libs/components/src/Placeholder/Placeholder.test.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Placeholder from './Placeholder'
+import { Col } from '@cwds/reactstrap'
 
 describe('Placeholder', () => {
   it('renders', () => {
     expect(() => {
       mount(<Placeholder />)
     }).not.toThrow()
+  })
+  it('renders a variable number of columns', () => {
+    let wrapper
+
+    wrapper = mount(<Placeholder cols={2} />)
+    expect(wrapper.find(Col).length).toBe(2)
+
+    wrapper = mount(<Placeholder cols={3} />)
+    expect(wrapper.find(Col).length).toBe(3)
+  })
+  it('renders a variable number of rows', () => {
+    let wrapper
+
+    wrapper = mount(<Placeholder cols={2} rows={4} tag="p" />)
+    expect(wrapper.find('p').length).toBe(8)
+
+    wrapper = mount(<Placeholder cols={3} rows={3} tag="p" />)
+    expect(wrapper.find('p').length).toBe(9)
   })
 })
