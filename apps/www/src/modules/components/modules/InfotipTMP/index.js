f0796b98ee129bb3904b77cf5aece832bd6e284b
Danny Eck <danny.eck@osi.ca.gov>
2018-12-28 16:03:30 -0800

    Another transient case change commit

--- apps/www/src/modules/components/modules/InfotipTMP/index.js
+++ apps/www/src/modules/components/modules/InfotipTMP/index.js
@@ -1,4 +1,2 @@
 /* eslint-disable import/no-webpack-loader-syntax */
-import InfoTipPage from '!babel-loader!@mdx-js/loader!./Infotip.mdx'
-
-export default InfoTipPage
+export { default } from '!babel-loader!@mdx-js/loader!./Infotip.mdx'
