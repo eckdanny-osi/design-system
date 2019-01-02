1c01959977f109c1c240d11301bc9b772621aa65
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-01-02 13:40:41 -0800

    fix rails app to validate js lib integration (#350)
    intentionally omitted from automatic changelog inclusion

--- apps/rails_app/app/javascript/main/Main.jsx
+++ apps/rails_app/app/javascript/main/Main.jsx
@@ -5,13 +5,16 @@ import {
   Avatar,
   Button,
   Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  CardFooter,
   Col,
-  Container,
   Page,
   Row,
 } from '@cwds/components'
 
-export const App = ({}) => (
+export const App = () => (
   <Page
     title="rails_app"
     layout="jumpnav"
@@ -23,98 +26,83 @@ export const App = ({}) => (
     sidenav={() => 'Sidenav'}
     main={() => (
       <div>
-        <main>
-          <Container>
-            <AlertUncontrolled color="warning">
-              <strong>Attention!</strong> This is not the guide site you're
-              looking for.
-            </AlertUncontrolled>
+        <AlertUncontrolled color="warning">
+          <strong>Attention!</strong> This is not the guide site you&apos;re
+          looking for.
+        </AlertUncontrolled>
+
+        <section>
+          <h2>About</h2>
+          <Card className="my-3">
+            <CardBody>
+              <p>
+                This is a sample app build with <tt>@cwds/components</tt> and{' '}
+                <tt>rails + webpacker</tt>. The app demonstrates integration and
+                may be used for automated test routines as well.
+              </p>
+              <p className="mb-0">
+                This is <strong>not</strong> a source of documentation! For
+                that, please refer to the{' '}
+                <a href="https://cws-cares.netlify.com">guide site</a>.
+              </p>
+            </CardBody>
+          </Card>
+        </section>
+
+        <section>
+          <h2>Components</h2>
+
+          <section>
+            <h3>Avatar</h3>
+            <Avatar />
+          </section>
+
+          <section>
+            <h3>Button</h3>
+            <Button>Hello</Button>
+            <br />
+            <Button color="success">Hello</Button>
+          </section>
+
+          <section>
+            <h3>Card</h3>
 
             <section>
-              <h2>About</h2>
-              <Card className="my-3">
-                <p>
-                  This is a sample app build with <tt>@cwds/components</tt> and{' '}
-                  <tt>rails + webpacker</tt>. The app demonstrates integration
-                  and may be used for automated test routines as well.
-                </p>
-                <p>
-                  This is <strong>not</strong> a source of documentation! For
-                  that, please refer to the{' '}
-                  <a href="http://design.eckdanny.com">guide site</a>.
-                </p>
+              <h4>Simple Card</h4>
+              <Card>
+                <CardBody>Just a simple Card</CardBody>
               </Card>
             </section>
 
             <section>
-              <h2>Components</h2>
-              <section>
-                <h3>Avatar</h3>
-                <Avatar />
-              </section>
-              <section>
-                <h3>Button</h3>
-                <Button>Hello</Button>
-                <br />
-                <Button color="success">Hello</Button>
-              </section>
-              <section>
-                <h3>Card</h3>
-                <section>
-                  <h4>Simple Card</h4>
-                  <Card>Just a simple Card</Card>
-                </section>
-                <section>
-                  <h4>Structured Card</h4>
-                  <Card>
-                    <Card.Header>Header</Card.Header>
-                    <Card.Body>Body</Card.Body>
-                    <Card.Footer>Footer</Card.Footer>
-                  </Card>
-                </section>
-                <section>
-                  <h4 className="text-danger">Multi-Context Card</h4>
-                  <p>TODO</p>
-                </section>
-              </section>
-              <section>
-                <h3>Collapse</h3>
-                {/* <Collapse /> */}
-              </section>
-              <section>
-                <h3>Grid</h3>
-                <Row>
-                  <Col sm="6">alksdjf</Col>
-                  <Col sm="6">alksdjf</Col>
-                </Row>
-                <Row>
-                  <Col sm="6">alksdjf</Col>
-                  <Col sm="6">alksdjf</Col>
-                </Row>
-              </section>
-              <section>
-                <h3>Icon</h3>
-                {/* <Icon /> */}
-              </section>
-              <section>
-                <h3>Logo</h3>
-                {/* <Logo /> */}
-              </section>
-              <section>
-                <h3>PageHeader</h3>
-                {/* <PageHeader /> */}
-              </section>
-              <section>
-                <h3>Tooltip</h3>
-                {/* <Tooltip /> */}
-              </section>
-              <section>
-                <h3>InfoTip</h3>
-                {/* <InfoTip /> */}
-              </section>
+              <h4>Structured Card</h4>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Header</CardTitle>
+                </CardHeader>
+                <CardBody>Body</CardBody>
+                <CardFooter>Footer</CardFooter>
+              </Card>
             </section>
-          </Container>
-        </main>
+
+            <section>
+              <h4 className="text-danger">Multi-Context Card</h4>
+              <p>TODO</p>
+            </section>
+          </section>
+
+          <section>
+            <h3>Grid</h3>
+            <Row>
+              <Col sm="6">alksdjf</Col>
+              <Col sm="6">alksdjf</Col>
+            </Row>
+            <Row>
+              <Col sm="6">alksdjf</Col>
+              <Col sm="6">alksdjf</Col>
+            </Row>
+          </section>
+        </section>
       </div>
     )}
   />
