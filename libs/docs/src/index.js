22e5bb7944b91e3b89391a8dd64498386933883b
Naga Pradeep Gajula <naga.gajula@osi.ca.gov>
2019-01-04 10:31:34 -0800

    move ReferenceCard component to docs

--- libs/docs/src/index.js
+++ libs/docs/src/index.js
@@ -1,3 +1,4 @@
 export { default as ColorPallete } from './ColorPallete'
 export { default as AnchorLink } from './AnchorLink'
 export { default as Figure } from './Figure'
+export { default as ReferenceCard } from './ReferenceCard'
