214d7cca36892f66b03dd2604bafe310e3d5e2f5
vikas199 <mvikasraj123@gmail.com>
2019-01-16 12:53:42 -0800

    added babel 7 to package json

--- babel.config-common.js
+++ babel.config-common.js
@@ -1,8 +1,8 @@
 module.exports = {
-  presets: ['env', 'react'],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
-    'transform-class-properties',
-    'transform-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
     [
       'transform-rename-import',
       {
