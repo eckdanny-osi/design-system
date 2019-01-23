343290e453442e4c6f9c24ae3ecccefd5721e7b7
Oleksii Khilkevych <oleksii.khilkevych@osi.ca.gov>
2019-01-23 15:46:15 -0800

    fix(InputMask): fix phone number example mask.

--- apps/www/src/modules/components/modules/InputMask/PhoneInputMask.example.jsx
+++ apps/www/src/modules/components/modules/InputMask/PhoneInputMask.example.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { InputMask } from '@cwds/components'
 
 const PhoneInputMaskExample = () => (
-  <InputMask mask='(999) 999-99-99' maskChar="_"/>
+  <InputMask mask='(999)999-9999' maskChar="_"/>
 )
 
 export default PhoneInputMaskExample
