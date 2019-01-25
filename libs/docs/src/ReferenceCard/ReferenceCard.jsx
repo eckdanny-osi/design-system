bcb52cc32b54ca8bf04fa5aff80fd3f8b70d5be1
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-01-25 14:15:17 -0800

    fix(docs): remove default margin on ReferenceCard (#380)

--- libs/docs/src/ReferenceCard/ReferenceCard.jsx
+++ libs/docs/src/ReferenceCard/ReferenceCard.jsx
@@ -3,15 +3,17 @@ import cn from 'classnames'
 import { Card, CardBody, Icon } from '@cwds/components'
 import Styles from './ReferenceCard.module.scss'
 
-export default ({ children, name = 'info-circle' }) => (
-  <Card className="my-3 bg-breath border-0 rounded-0">
-    <CardBody className="d-flex align-items-start">
-      <Icon name={name} size="2x" color="info" className="mr-3" />
-      <div
-        className={cn('flex-grow-1 my-0 font-italic text-info', Styles.Body)}
-      >
-        {children}
-      </div>
-    </CardBody>
-  </Card>
-)
+export default ({ children, className, name = 'info-circle' }) => {
+  return (
+    <Card className={cn('bg-breath border-0 rounded-0', className)}>
+      <CardBody className="d-flex align-items-start">
+        <Icon name={name} size="2x" color="info" className="mr-3" />
+        <div
+          className={cn('flex-grow-1 my-0 font-italic text-info', Styles.Body)}
+        >
+          {children}
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
