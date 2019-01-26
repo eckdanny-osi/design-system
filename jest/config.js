39c29bdfa5159b5175a11c76ab7e3401d36cd26c
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-01-25 22:15:42 -0800

    chore(build): use jest config for setup files (#385)

--- jest/config.js
+++ jest/config.js
@@ -2,7 +2,7 @@ module.exports = ({ project }) => ({
   name: project,
   displayName: project,
   verbose: true,
-  setupTestFrameworkScriptFile: '<rootDir>/../../setupTests.js',
+  setupFilesAfterEnv: ['<rootDir>/../../setupTests.js'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
