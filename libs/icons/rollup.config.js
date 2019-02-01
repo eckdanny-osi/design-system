0e0701364025d0c78dd2076d533b6256774ca2d9
Danny Eck <danny.eck@osi.ca.gov>
2019-01-31 19:58:49 -0800

    fix(build): consolidate babel, jest, rollup config

--- libs/icons/rollup.config.js
+++ libs/icons/rollup.config.js
@@ -1,46 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
+import { mkRollupConfig } from '../../scripts/rollup-utils'
 import pkg from './package.json'
-import { depsBlacklist } from '../../scripts/build-utils'
 
-export default [
-  {
-    input: 'src/index.js',
-    output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        exports: 'named',
-        sourcemap: true,
-      },
-      {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-    external: id => {
-      if (depsBlacklist(pkg).includes(id)) return true
-      if (/module\.s?css$/.test(id)) return true
-      return false
-    },
-    plugins: [
-      resolve({
-        extensions: ['.js', '.jsx'],
-      }),
-      commonjs(),
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-        presets: [['@babel/preset-env', { modules: false }]],
-        plugins: [
-          '@babel/plugin-proposal-class-properties',
-          '@babel/plugin-proposal-object-rest-spread',
-          '@babel/plugin-transform-react-jsx',
-          '@babel/plugin-external-helpers',
-        ],
-      }),
-    ],
-  },
-]
+module.exports = mkRollupConfig(pkg)
---
+++ scripts/rollup-utils.js
@@ -0,0 +1,67 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+
+const REGEXP_STYLE_MODULE = /module\.s?css$/
+
+/**
+ * Predicate filter fn to return distinct elements
+ * @private
+ * @param {*} value
+ * @param {number} index
+ * @param {*[]} array
+ */
+function distinct(value, index, array) {
+  return array.indexOf(value) === index
+}
+
+/**
+ * Extract distinct deps and peerDeps from pkgJson
+ * @param {Object} pkgJson
+ * @param {Object} pkgJson.dependencies
+ * @param {Object} pkgJson.peerDependencies
+ */
+function depsBlacklist({ dependencies = {}, peerDependencies = {} }) {
+  return [
+    ...Object.keys(dependencies),
+    ...Object.keys(peerDependencies),
+  ].filter(distinct)
+}
+
+export function mkRollupConfig(pkg, opts = {}) {
+  const externals = depsBlacklist(pkg)
+  return {
+    input: 'src/index.js',
+    external: id => {
+      if (externals.includes(id)) return true
+      if (REGEXP_STYLE_MODULE.test(id)) return true
+      return false
+    },
+    output: {
+      file: pkg.main,
+      dir: 'dist',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      // postcss({
+      //   extract: 'dist/styles.module.css',
+      //   extensions: ['.scss', '.css'],
+      //   sourceMap: true,
+      //   autoModules: true,
+      //   // onImport: (...args) => {
+      //   // console.log(args)
+      //   // },
+      // }),
+      resolve({
+        extensions: ['.js', '.jsx'],
+      }),
+      commonjs(),
+      babel({
+        rootMode: 'upward',
+      }),
+    ],
+    ...opts,
+  }
+}
