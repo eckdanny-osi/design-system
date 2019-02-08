83d6991b385dd29fff66d2ef062126796e669815
Danny Eck <danny.eck@osi.ca.gov>
2019-02-08 02:44:06 -0800

    fix build

--- libs/docs/src/Articles/index.js
+++ libs/docs/src/Articles/index.js
@@ -1,5 +1,5 @@
 import * as Utils from './article-router'
 export { default as Article } from './Article'
-export { default as SideNav } from './SideNav'
-export { default as NotFound } from './NotFound'
+export { default as SideNav } from './ArticleSideNav'
+export { default as NotFound } from './ArticleNotFound'
 export { Utils }
