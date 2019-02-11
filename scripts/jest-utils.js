b375a84face5fc0b154d02dd6245748051364ae6
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-02-11 10:55:25 -0800

    fix(tech): Transform cwds pkg imports in Jest tests (#408)
    * fix npm clean libs name
    
    extracted to https://github.com/ca-cwds/design-system/pull/407
    
    * fix(tech): transform cwds pkgs in Jest tests
    
    Maps `@cwds/*` imports to `$1/src` so that we don't have to build before
    downstream libs can run tests. :)

--- scripts/jest-utils.js
+++ scripts/jest-utils.js
@@ -16,6 +16,7 @@ function getDefault() {
       '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
         '<rootDir>/__mocks__/fileMock.js',
       '\\.(css|scss)$': 'identity-obj-proxy',
+      '^@cwds\\/([^/]+)': '<rootDir>/../$1/src',
     },
     transform: {
       '^.+\\.jsx?$': 'babel-jest',
