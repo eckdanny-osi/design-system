f06dabe4632330f5342900878eb47f93071d1735
Karthik Sista <30814092+karthik-sista@users.noreply.github.com>
2019-02-20 14:00:28 -0800

    CL 52  coded prototype reassessment (#423)
    * Added reassessment page and route.
    
    * Added AssesmentHistoryTile and Datagrid.
    
    * Small Fix.
    
    * Clean folder structure.
    
    * Added ClientInfo Card.
    
    * Added ellipsisand links on the table.

---
+++ apps/www/src/demos/ReassessmentForm/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Breadcrumb, BreadcrumbItem, IconButton, Page } from '@cwds/components'
+
+import ClientInfo from './ClientInfo'
+import AssesmentHistory from './AssesmentHistory'
+
+export default () => (
+  <Page
+    title="CANS"
+    Breadcrumb={
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <Link to="/">Dashboard</Link>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Link to="/">Client Search</Link>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>Smith, Sarah</BreadcrumbItem>
+      </Breadcrumb>
+    }
+    PageActions={
+      <IconButton icon="search" size="sm">
+        CLIENT SEARCH
+      </IconButton>
+    }
+    main={() => (
+      <div>
+        <ClientInfo />
+        <AssesmentHistory />
+      </div>
+    )}
+  />
+)
