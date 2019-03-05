546cb63595db5ae90a67631baec24a0049a4a379
karthik-sista <karthik.sista@osi.ca.gov>
2019-03-05 08:28:01 -0800

    Added Message placeholder in demo.

---
+++ apps/www/src/demos/MesssageArea/Sidenav.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { JumpNav } from '@cwds/components'
+
+const routes = [{ title: 'Message Area', path: '#messagearea' }]
+
+export default () => <JumpNav routes={routes} />
