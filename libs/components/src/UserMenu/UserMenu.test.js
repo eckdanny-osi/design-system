cf9db36e788019e9d13a8364f9ff23aa2351b089
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-03-08 11:28:43 -0800

    fix(components): fix UserMenu support for Avatar (#448)
    The UserMenu now accepts a prop for Avatar. Updates docs (www). Removes an old? (unexported) UserMenu implementation. Adds tests.
    
    squashed commits:
    
    * refactor UserMenu
    * remove unused UserMenu implementation
    * update Menu article in www

--- libs/components/src/UserMenu/UserMenu.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import UserMenu from './UserMenu'
-
-describe('UserMenu', () => {
-  it('renders', () => {
-    expect(() => mount(<UserMenu />)).not.toThrow()
-  })
-})
