16121c70fc4c2b41f68a4a930173cd730b228860
Danny Eck <danny.eck@osi.ca.gov>
2019-03-08 09:42:23 -0800

    refactor UserMenu

--- libs/components/src/Menu/UserMenu.test.js
+++ libs/components/src/Menu/UserMenu.test.js
@@ -1,27 +1,36 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import UserMenu from './UserMenu'
 import Menu from './Menu'
+import DropdownToggle from 'reactstrap/lib/DropdownToggle'
 import Avatar from '../Avatar'
 
 describe('UserMenu', () => {
   it('wraps Menu', () => {
     const wrapper = shallow(<UserMenu label="Boaty McBoatface" />)
-    expect(wrapper.find(Menu).length).toBe(1)
+    expect(wrapper.type()).toBe(Menu)
   })
   it('renders label in the popup button', () => {
-    const wrapper = mount(
-      <UserMenu label="Boaty McBoatface" toggle={() => {}} />
-    )
-    const label = wrapper.find('button[aria-haspopup] span')
-    expect(label.text()).toEqual('Boaty McBoatface')
-    expect(label.hasClass('text-capitalize')).toBe(true)
+    const wrapper = shallow(<UserMenu label="Boaty McBoatface" />).dive()
+    expect(
+      wrapper
+        .find(DropdownToggle)
+        .children()
+        .text()
+    ).toContain('Boaty McBoatface')
   })
-  it('renders an Avatar in the popup button', () => {
-    const wrapper = mount(
-      <UserMenu label="Boaty McBoatface" toggle={() => {}} />
-    )
+  it('renders a default Avatar in the popup button', () => {
+    const wrapper = shallow(<UserMenu label="Boaty McBoatface" />).dive()
+    expect(wrapper.find(Avatar).length).toBe(1)
     expect(wrapper.find(Avatar).prop('size')).toBe('sm')
     expect(wrapper.find(Avatar).prop('invert')).toBe(true)
+  })
+  it('accepts a custom avatar', () => {
+    const myAvatar = <Avatar test-id="test" />
+    const wrapper = shallow(<UserMenu avatar={myAvatar} label="test" />).dive()
+    expect(wrapper.find(Avatar).length).toBe(1)
+    expect(wrapper.find(Avatar).prop('size')).toBe('sm')
+    expect(wrapper.find(Avatar).prop('invert')).toBe(true)
+    expect(wrapper.find(Avatar).prop('test-id')).toBe('test')
   })
 })
