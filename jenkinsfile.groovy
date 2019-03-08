ce14d55f16180922747e7c9b51891c5b1f8615ea
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-03-08 14:56:03 -0800

    chore(build): add build pipeline (#455)
    * FIT-475 - Create Master Pipeline for Component Library: checking this repository access
    * FIT-475 - Create Master Pipeline for Component Library: initial pipeline

---
+++ jenkinsfile.groovy
@@ -0,0 +1,73 @@
+import groovy.transform.Field
+
+@Library('jenkins-pipeline-utils') _
+
+switch(env.BUILD_JOB_TYPE) {
+  case 'master': buildMaster(); break;
+  case 'release':releasePipeline(); break;
+  default: buildPullRequest();
+}
+
+def buildPullRequest() {
+  node('linux') {
+    def triggerProperties = githubPullRequestBuilderTriggerProperties()
+    properties([
+      githubConfig(),
+      pipelineTriggers([triggerProperties]),
+      buildDiscarderDefaults()
+    ])
+    try {
+      checkoutStage()
+      checkForLabel()
+    } catch(Exception exception) {
+      currentBuild.result = 'FAILURE'
+      throw exception
+    } finally {
+      cleanupStage()
+    }
+  }
+}
+
+def buildMaster() {
+  node('linux') {
+    triggerProperties = pullRequestMergedTriggerProperties('design-system-master')
+    properties([
+      githubConfig(),
+      pipelineTriggers([triggerProperties]),
+      buildDiscarderDefaults('master')
+    ])
+
+    try {
+      checkoutStage()
+      checkForLabel()
+    } catch(Exception exception) {
+      currentBuild.result = 'FAILURE'
+      throw exception
+    } finally {
+      cleanupStage()
+    }
+  }
+}
+
+def checkoutStage() {
+  stage('Checkout') {
+    deleteDir()
+    checkout scm
+  }
+}
+
+def checkForLabel() {
+  stage('Verify SemVer Label') {
+    checkForLabel('design-system')
+  }
+}
+
+def cleanupStage() {
+  stage('Cleanup') {
+    cleanWs()
+  }
+}
+
+def githubConfig() {
+  githubConfigProperties('https://github.com/ca-cwds/design-system')
+}
