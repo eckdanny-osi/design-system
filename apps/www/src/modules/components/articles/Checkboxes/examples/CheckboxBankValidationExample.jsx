37f127ed4192404468c992cdae0b6e34b243d019
Danny Eck <danny.eck@osi.ca.gov>
2019-03-19 11:45:46 -0700

    WiP

--- apps/www/src/modules/components/articles/Checkboxes/examples/CheckboxBankValidationExample.jsx
+++ apps/www/src/modules/components/articles/Checkboxes/examples/CheckboxBankValidationExample.jsx
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react'
 import { Formik, Form, Field, FieldArray } from 'formik'
+import cn from 'classnames'
 import { Label, Card, CardHeader, CardTitle, CardBody } from '@cwds/components'
 import { Fieldset, Legend, CheckboxControl } from '@cwds/forms'
+import { Select } from '@cwds/components'
+import { FormGroup, FormFeedback } from '@cwds/reactstrap'
 
 const OPTIONS_BEATLES = [
   {
@@ -26,8 +29,8 @@ function validateBeatles(value) {
   // let error
   const errors = {}
 
-  if (value.beatle.filter(Boolean).length === 4) {
-    errors.beatles = 'You can not choose them all!'
+  if (Object.values(value.favoriteBeatle).filter(Boolean).length === 4) {
+    errors.favoriteBeatle = 'You can not choose them all!'
   }
 
   return errors
@@ -39,51 +42,93 @@ const CheckboxBankValidationExample = () => (
       <CardTitle>With Validation</CardTitle>
     </CardHeader>
     <CardBody>
-      <Formik
-        initialValues={{
-          beatle: [],
-          color: '',
-          something: '',
-        }}
-        onSubmit={values => console.log(values)}
-        validate={validateBeatles}
-      >
-        {({ errors, touched, isValidating, values }) => {
-          // console.log(values)
-          return (
-            <div>
-              <Field
-                name="beatle"
-                render={({ field, form, ...restProps }) => {
-                  // const { onChange, onBlur, name, value } = field
-                  // console.log('Field#render()', { field })
-                  // console.log(errors)
-                  return (
-                    <Fieldset>
-                      <Legend>Favorite Beatle</Legend>
-                      {errors.beatles && <div>{errors.beatles}</div>}
-                      {OPTIONS_BEATLES.map(({ label, value }, i) => {
-                        // console.log({ value, label })
-                        return (
-                          <CheckboxControl
-                            id={`${field.name}__${i}`}
-                            key={value}
-                            name={`${field.name}[${i}]`}
-                            value={value === field.value[i]}
-                            onChange={e => {
-                              // const { value, name, checked } = e.target
-                              // console.log({ name, value, checked })
-                              field.onChange(e)
-                            }}
-                            label={label}
-                          />
-                        )
-                      })}
-                    </Fieldset>
-                  )
-                }}
-              />
-              <Field
+      <Form>
+        <Formik
+          initialValues={{
+            favoriteBeatle: {
+              '2': true,
+            },
+            color: '',
+            iceCream: 'strawberry',
+            favoriteOgreSaying: '',
+          }}
+          onSubmit={values => console.log(values)}
+          validate={validateBeatles}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            isValidating,
+            setFieldTouched,
+            setFieldValue,
+          }) => {
+            // console.log(values)
+            return (
+              <div>
+                <Field
+                  name="favoriteBeatle"
+                  render={({ field, form, ...restProps }) => {
+                    const {
+                      onChange,
+                      onBlur,
+                      name: fmkName,
+                      value: fmkValue,
+                    } = field
+                    // console.log('Field#render()', { field })
+                    // console.log(errors)
+                    return (
+                      <Fieldset>
+                        <Legend>Favorite Beatle</Legend>
+                        <div
+                          className={cn('px-3', {
+                            'py-3 border border-danger': !!errors.favoriteBeatle,
+                          })}
+                        >
+                          {errors.favoriteBeatle && (
+                            <FormFeedback
+                              valid={false}
+                              className="d-block mb-3"
+                            >
+                              {errors.favoriteBeatle}
+                            </FormFeedback>
+                          )}
+                          {OPTIONS_BEATLES.map(({ label, value }, i) => {
+                            // console.log({ value, label })
+                            // debugger
+                            // console.log({ fmkName, fmkValue, label, value })
+                            return (
+                              <CheckboxControl
+                                id={`${field.name}__${value}`}
+                                key={`${field.name}__${value}`}
+                                name={`${field.name}[${value}]`}
+                                value={value}
+                                defaultChecked={field.value[value]}
+                                onChange={e => {
+                                  setFieldTouched(`${field.name}`)
+                                  field.onChange(e)
+                                }}
+                                onBlur={e => setFieldTouched(`${field.name}`)}
+                                // onChange={e => {
+                                //   const { name, value, checked } = e.target
+                                //   console.log({ name, value, checked })
+                                //   // const { value, name, checked } = e.target
+                                //   // console.log({ name, value, checked })
+                                //   field.onChange(e)
+                                // }}
+                                label={label}
+                                labelClassName={
+                                  !!errors.favoriteBeatle && 'text-danger'
+                                }
+                              />
+                            )
+                          })}
+                        </div>
+                      </Fieldset>
+                    )
+                  }}
+                />
+                {/* <Field
                 name="color"
                 component="select"
                 placeholder="Favorite Color"
@@ -91,72 +136,108 @@ const CheckboxBankValidationExample = () => (
                 <option value="red">Red</option>
                 <option value="green">Green</option>
                 <option value="blue">Blue</option>
-              </Field>
-              <br />
-              <Label>Ogres Say</Label>
-              <Field
-                name="something"
-                render={({ field, form, ...props }) => {
-                  const { onChange, onBlur, name, value } = field
-                  return (
-                    <select
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      name={name}
-                      value={value}
-                      {...props}
-                    >
-                      <option value="fee">Fee</option>
-                      <option value="fi">Fi</option>
-                      <option value="fo">Fo</option>
-                      <option value="fum">Fum</option>
-                      <option value="all">All of the Above</option>
-                    </select>
-                  )
-                }}
-              />
-              <br />
-              <pre>{JSON.stringify(values, null, 2)}</pre>
-            </div>
+              </Field> */}
+                <Field
+                  name="iceCream"
+                  render={({ field, form, ...props }) => {
+                    // console.log({ field, props })
+                    const options = [
+                      { value: 'chocolate', label: 'Chocolate' },
+                      { value: 'strawberry', label: 'Strawberry' },
+                      { value: 'vanilla', label: 'Vanilla' },
+                    ]
+                    return (
+                      <FormGroup>
+                        <Label>IceCream</Label>
+                        <Select
+                          // {...field}
+                          name={field.name}
+                          // {...props}
+                          options={options}
+                          defaultValue={options.find(
+                            d => d.value === field.value
+                          )}
+                          // value={options.find(d => d.value === field.value)}
+                          onChange={(opt, { name }) => {
+                            // console.log({ one, action })
+                            // field.onChange(action.name, one.value)
+                            setFieldValue(name, opt.value)
+                            setFieldTouched(name)
+                          }}
+                        />
+                      </FormGroup>
+                    )
+                  }}
+                />
+                <br />
+                <FormGroup>
+                  <Label>Ogres Say</Label>
+                  <Field
+                    name="favoriteOgreSaying"
+                    render={({ field, form, ...props }) => {
+                      const { onChange, onBlur, name, value } = field
+                      return (
+                        <select
+                          onChange={onChange}
+                          onBlur={onBlur}
+                          name={name}
+                          value={value}
+                          {...props}
+                        >
+                          <option value="fee">Fee</option>
+                          <option value="fi">Fi</option>
+                          <option value="fo">Fo</option>
+                          <option value="fum">Fum</option>
+                          <option value="all">All of the Above</option>
+                        </select>
+                      )
+                    }}
+                  />
+                </FormGroup>
+                <pre>
+                  {JSON.stringify({ values, errors, touched }, null, 2)}
+                </pre>
+              </div>
 
-            // <Fragment>
-            //   <Field name="friends[0]" />
-            //   <Field name="friends[1]" />
-            // </Fragment>
-          )
-          // console.log('Formkik rendering...')
-          // return (
-          //   <FieldArray
-          //     name="beatles"
-          //     render={({ push, remove }) => (
-          //       <Fieldset>
-          //         <Legend>Favorite Beatle</Legend>
-          //         {errors.beatles && touched.beatles && (
-          //           <div>{errors.beatles}</div>
-          //         )}
-          //         {OPTIONS_BEATLES.map(({ label, value }) => (
-          //           <CheckboxControl
-          //             key={value}
-          //             id={`beatles__${label}`}
-          //             name="beatles"
-          //             label={label}
-          //             value={value}
-          //             checked={values.beatles.includes(value)}
-          //             onChange={e => {
-          //               if (e.target.checked) push(value)
-          //               else {
-          //                 const idx = values.beatles.indexOf(value)
-          //                 remove(idx)
-          //               }
-          //             }}
-          //           />
-          //         ))}
-          //       </Fieldset>
-          //     )}
-          //   />
-          // )
-        }}
-      </Formik>
+              // <Fragment>
+              //   <Field name="friends[0]" />
+              //   <Field name="friends[1]" />
+              // </Fragment>
+            )
+            // console.log('Formkik rendering...')
+            // return (
+            //   <FieldArray
+            //     name="beatles"
+            //     render={({ push, remove }) => (
+            //       <Fieldset>
+            //         <Legend>Favorite Beatle</Legend>
+            //         {errors.beatles && touched.beatles && (
+            //           <div>{errors.beatles}</div>
+            //         )}
+            //         {OPTIONS_BEATLES.map(({ label, value }) => (
+            //           <CheckboxControl
+            //             key={value}
+            //             id={`beatles__${label}`}
+            //             name="beatles"
+            //             label={label}
+            //             value={value}
+            //             checked={values.beatles.includes(value)}
+            //             onChange={e => {
+            //               if (e.target.checked) push(value)
+            //               else {
+            //                 const idx = values.beatles.indexOf(value)
+            //                 remove(idx)
+            //               }
+            //             }}
+            //           />
+            //         ))}
+            //       </Fieldset>
+            //     )}
+            //   />
+            // )
+          }}
+        </Formik>
+      </Form>
     </CardBody>
   </Card>
 )
--- libs/forms/src/CheckboxControl.tsx
+++ libs/forms/src/CheckboxControl.tsx
@@ -1,49 +1,59 @@
-import { IOption } from "./types";
-import React, { Fragment } from "react";
-import { FormGroup, Input, Label, Util } from "@cwds/reactstrap";
-import cn from "classnames";
+import { IOption } from './types'
+import React, { Fragment } from 'react'
+import { FormGroup, Input, Label, Util } from '@cwds/reactstrap'
+import cn from 'classnames'
 
-const mapToCssModules = Util.mapToCssModules;
+const mapToCssModules = Util.mapToCssModules
 
 // console.log(mapToCssModules())j
 
-console.log(mapToCssModules("form-check-input"));
+console.log(mapToCssModules('form-check'))
 
 interface CheckboxControlProps {
-  id?: string;
-  className?: string;
-  inline?: boolean;
-  name: string;
-  label: string;
-  value: string | boolean | number;
-  checked: boolean;
-  disabled: boolean;
-  onChange?: React.FormEventHandler;
-  onBlur?: React.FormEventHandler;
+  id?: string
+  className?: string
+  labelClassName?: string
+  inline?: boolean
+  name: string
+  label: string
+  value: string | boolean | number
+  checked: boolean
+  defaultChecked: boolean
+  disabled: boolean
+  onChange?: React.FormEventHandler
+  onBlur?: React.FormEventHandler
 }
+
+// form-check-inline .form-check-input { margin-right: 0.375rem }
 
 const CheckboxControl = (props: CheckboxControlProps) => {
   return (
-    <FormGroup check inline={props.inline} className={cn(props.className)}>
+    <FormGroup
+      check
+      inline={props.inline}
+      // className={cn(props.className)}
+    >
       <Input
         type="checkbox"
         id={props.id}
         name={props.name}
         checked={props.checked}
-        className={mapToCssModules(cn("form-check-input"))}
+        defaultChecked={props.defaultChecked}
+        // className={mapToCssModules(cn('form-check-input'))}
+        className="form-check-input"
         disabled={props.disabled}
         value={String(props.value)}
         onChange={props.onChange}
         onBlur={props.onBlur}
       />
       <Label
-        className={mapToCssModules(cn("form-check-label"))}
+        className={cn('form-check-label', props.labelClassName)}
         htmlFor={props.id}
       >
         {props.label}
       </Label>
     </FormGroup>
-  );
-};
+  )
+}
 
-export default CheckboxControl;
+export default CheckboxControl
