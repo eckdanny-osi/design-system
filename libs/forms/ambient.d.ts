c1cda2b8af3c53e417136fbfec09366ae7e597f5
Danny Eck <danny.eck@osi.ca.gov>
2019-03-20 17:06:06 -0700

    fix TSC whining about scss modules

--- libs/forms/ambient.d.ts
+++ libs/forms/ambient.d.ts
@@ -1,3 +1,8 @@
 declare module '@cwds/reactstrap' {
   export { Input, FormGroup, Label } from 'reactstrap'
 }
+
+declare module '*.scss' {
+  const content: { [className: string]: string }
+  export default content
+}
